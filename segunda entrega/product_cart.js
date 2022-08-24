7aa3c5282c4e70d64cc8efad8d0fbef0a2de8d12
Carlos Monsalve <88389975+monsalvechs@users.noreply.github.com>
2022-08-23 21:40:37 -0300

    seguyda entegra del proyecto final.
    se incorpora al log in, una lista de productos y JavaScript para la realización de un carrito de compras.

---
+++ segunda entrega/product_cart.js
@@ -0,0 +1,49 @@
+/*class Producto {
+    constructor(nombre, descripcion, precio ) {
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.precio = precio
+    }
+}
+
+let productos = []
+
+if (localStorage.getItem("productos")){
+productos = JSON.parse(localStorage.getItem("productos"))
+}else{
+    localStorage.setItem("productos", JSON.stringify(productos))
+}*/
+
+
+const contenedorProductos = document.getElementById("contendor-productos");
+let carrito = []
+
+stock.forEach((producto) => {
+    const div = document.createElement("div")
+    
+    div.innerHTML = `
+                    <div class="card" style="width: 18rem;">
+                      <img src=${producto.img} class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">${producto.descripcion}</p>
+                        <button id="agregar${producto.id}" href="#" class="btn btn-primary">$${producto.precio}</button>
+                      </div>
+`
+
+    contenedorProductos.appendChild(div)
+
+const boton = document.getElementById("agregar" + producto.id)
+
+boton.addEventListener('click', () => {
+    agregarCarrito(producto.id)
+})
+
+
+})
+
+const agregarCarrito = (prodId) => {
+    const item = stock.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    console.log (carrito)
+}
---
+++ segunda entrega/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form").addEventListener('submit', validarFormulario); 
+  });
+
+class usuario {
+    constructor(mail, password) {
+        this.mail = mail
+        this.password = password
+    }
+}
+
+const form = document.getElementById("form");
+const usuarios = [];//array donde se guardan los usuarios que se loggean
+
+
+function validarFormulario(e) {
+    e.preventDefault();
+    const email = document.getElementById("inputEmail").value
+    const password = document.getElementById("inputPassword").value
+
+    const user = new usuario(email, password)
+
+    usuarios.push(user)
+
+    form.reset()
+    console.log(usuarios)
+    
+    
+    localStorage.setItem('User-Logged', JSON.stringify(usuarios.mail));
+
+    this.submit();
+  }
